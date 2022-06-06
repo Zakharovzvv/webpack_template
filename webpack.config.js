baa0671f9a5e392b86d5a5c1ce5078fc5e881a84
Vladimir <cAB-8:VF>
2022-06-06 11:22:43 +0300

    added resolve extentions

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,18 @@
 const path = require('path')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
 const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const TerserWebpackPlugin = require('terser-webpack-plugin')
+const {resolve} = require("@babel/core/lib/vendor/import-meta-resolve");
 
 module.exports = {
     mode: "development",
-    entry: ["@babel/polyfill","./src/index.ts"],
+    entry: ["@babel/polyfill", "./src/index.ts"],//update entry point file name
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "[name].[hash].js"
+    },
+    resolve: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"]
     },
     devServer: {
         port: 3000
@@ -26,16 +31,20 @@ module.exports = {
                 test: /\.(jpg|jpeg|png|svg)/,
                 use: ['file-loader']
             },
-             {
-                test: /\.m?ts$/,
+            {
+                test: /\.(js|ts|jsx|tsx)$/,
                 exclude: /node_modules/,
                 use: {
                     loader: "babel-loader",
                     options: {
-                        presets: ['@babel/preset-env',"@babel/preset-react","@babel/preset-typescript"]
+                        presets: ['@babel/preset-env', "@babel/preset-react", "@babel/preset-typescript"]
                     }
                 }
             },
         ]
-    }
+    },
+    optimization: {
+        minimize: false,
+        minimizer: [new TerserWebpackPlugin()],
+    },
 }
